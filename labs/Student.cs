7ff6f19f0a07401d6806fb3b998fff8091371730
ziad ayman <ziadayman@gmail.com>
2025-03-15 15:14:46 +0200

    lab4

---
+++ labs/Student.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace labs
+{
+    class Student
+    {
+        public int SSN { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Address {  get; set; }
+
+        public Student() { SSN = 0; Name = "NO Name";Age = 0;Address = "NO Address"; }
+
+        public Student(int sSN, string name, int age, string address)
+        {
+            SSN = sSN;
+            Name = name;
+            Age = age;
+            Address = address;
+        }
+        public void Print() 
+        {
+            Console.WriteLine($"SSN : {SSN} \nName : {Name}\nAge : {Age}\nAddress : {Address}");
+
+        }
+    }
+}
